959e032cfa029ced1c478677ba17c0a0685d0ba8
Corey <coreydihel@gmail.com>
2015-10-28 21:38:23 -0800

    Messing around with crypto

--- testing/Test.go
+++ testing/Test.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
 
 func main(){
-	fmt.Println("Working")
+	s := "Password"
+	p, err := bcrypt.GenerateFromPassword([]byte(s),bcrypt.DefaultCost)
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(bcrypt.CompareHashAndPassword(p,[]byte("Password")))
+	fmt.Println(bcrypt.CompareHashAndPassword(p,[]byte("Passwordadfadf")))
+	fmt.Println(bcrypt.ErrMismatchedHashAndPassword)
 }
