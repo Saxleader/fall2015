68bbd836a5e309190d1fc551e752ed1ac370f5e6
Corey <coreydihel@gmail.com>
2015-11-12 11:53:49 -0800

    Finished Presentation 10 Exercises

---
+++ PresExercises/10/slide36/main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/saxleader/fall2015/PresExercises/10/slide36/names"
+)
+
+func main() {
+	fmt.Println("Hello",names.MyName)
+}
---
+++ PresExercises/10/slide36/names/names.go
@@ -0,0 +1,9 @@
+package names
+
+var MyName string
+var yourName string
+
+func init() {
+	MyName = "Corey"
+	yourName = "Todd"
+}
